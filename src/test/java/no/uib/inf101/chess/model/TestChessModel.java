3bee0cd23c0e5e4506a1d4c97c67efe28dce0648
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-04-17 15:49:42 +0200

    Tests in model

--- src/test/java/no/uib/inf101/chess/model/TestChessModel.java
+++ src/test/java/no/uib/inf101/chess/model/TestChessModel.java
@@ -16,6 +16,11 @@ public class TestChessModel {
 
         assertNotNull(model);
         assertNotNull(model.getBoard());
+        assertEquals(GameState.ACTIVE, model.getGameState());
+        assertEquals(null, model.getSelectedSquare());
+        assertEquals(null, model.getLastMoveFrom());
+        assertEquals(null, model.getLastMoveTo());
+        assertEquals(null, model.getWinner());
     }
 
     @Test
@@ -68,5 +73,41 @@ public class TestChessModel {
 
         assertEquals(moveFromSquare, model.getLastMoveFrom());
         assertEquals(moveToSquare, model.getLastMoveTo());
+    }
+
+    @Test
+    void checkMateTest() {
+        ChessModel model = new ChessModel();
+
+        // Move white pawn to E4
+        model.setSelectedSquare(model.getBoard().get(Column.E, 2));
+        model.setSelectedSquare(model.getBoard().get(Column.E, 4));
+
+        // Move black pawn to E5
+        model.setSelectedSquare(model.getBoard().get(Column.E, 7));
+        model.setSelectedSquare(model.getBoard().get(Column.E, 5));
+
+        // Move white bishop to E4
+        model.setSelectedSquare(model.getBoard().get(Column.F, 1));
+        model.setSelectedSquare(model.getBoard().get(Column.C, 4));
+
+        // Move black pawn to D6
+        model.setSelectedSquare(model.getBoard().get(Column.D, 7));
+        model.setSelectedSquare(model.getBoard().get(Column.D, 6));
+
+        // Move white queen to H5
+        model.setSelectedSquare(model.getBoard().get(Column.D, 1));
+        model.setSelectedSquare(model.getBoard().get(Column.H, 5));
+
+        // Move black knight to E4
+        model.setSelectedSquare(model.getBoard().get(Column.G, 8));
+        model.setSelectedSquare(model.getBoard().get(Column.F, 6));
+
+        // Move white queen to F7 and set black checkmate
+        model.setSelectedSquare(model.getBoard().get(Column.H, 5));
+        model.setSelectedSquare(model.getBoard().get(Column.F, 7));
+
+        assertEquals(GameState.CHECKMATE, model.getGameState());
+        assertEquals(ChessColor.WHITE, model.getWinner());
     }
 }
